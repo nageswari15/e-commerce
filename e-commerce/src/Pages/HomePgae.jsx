cb62a907904554c550b12af9f316e949e0250447
nageswari15 <sivanageswariputta@gmail.com>
2026-06-07 12:34:49 +0530

    use data form backend to generate hTML

--- e-commerce/src/Pages/HomePgae.jsx
+++ e-commerce/src/Pages/HomePgae.jsx
@@ -1,16 +1,19 @@
 import axios from 'axios';
+import {useEffect,useState} from 'react';
 import './HomePage.css';
 import {Header} from '../components/Header';
-import {products} from '../../Starting-code/data/products';
+
 import { Link } from 'react-router-dom';
  
  export function HomePage() {
-    axios.get('http://localhost:3000/api/products')
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+         axios.get('http://localhost:3000/api/products')
     .then((response) => {
-        console.log(response.data);
+       setProducts(response.data);
     });
+    }, [])
    
-
 
     return (
     <>
